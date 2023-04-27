5011d284e9ac61f1bfdd56011d0b6b218c9c41b1
IdoyaGE <igearkabaia@gmail.com>
2023-04-27 10:45:46 +0200

    commit con funcionalidad de botones de eventos

--- eventos/src/App.js
+++ eventos/src/App.js
@@ -1,17 +1,19 @@
 import logo from './logo.svg';
+import {useState} from"react";
 import './App.css';
 import EventTypeList from './components/EventTypeList';
 import EventList from './components/EventList';
-
+//Utilizamos los props en EventTypeList y EventList
 function App() {
-  return (
+const [eventType, setEventType]= useState(0);
+    return (
     <div className="App">
       <header>
         < h1>Eventos</h1>
       </header>
-      <main>
-        <EventTypeList/>
-        <EventList/>
+      <main> 
+        <EventTypeList handleClick={setEventType} selectedType = {eventType}/>
+        <EventList eventType={eventType}/>
       </main>
     </div>
   );
